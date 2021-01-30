6d217a540f657b6fda4a20c0e512c3f8f042fd48
muecahid <m-a-yeniguen@hotmail.com>
2021-01-30 17:06:07 +0100

    Add layouts for the three slottypes

--- app/OutWait/app/src/main/java/elite/kit/outwait/recyclerviewSetUp/viewHolder/PauseSlotViewHolder.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/recyclerviewSetUp/viewHolder/PauseSlotViewHolder.kt
@@ -1,7 +1,6 @@
 package elite.kit.outwait.recyclerviewSetUp.viewHolder
 
 import android.view.View
-import androidx.appcompat.view.menu.MenuView
 import elite.kit.outwait.waitingQueue.timeSlotModel.Pause
 
 class PauseSlotViewHolder(private var itemView: View):BaseViewHolder<Pause>(itemView) {
