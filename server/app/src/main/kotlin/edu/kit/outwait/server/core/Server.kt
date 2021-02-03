378939cca3a149d44348111a9a751dc0b5be626a
Erik Götzfried <erik.goetzfried@gmail.com>
2021-02-03 11:22:52 +0100

    Make server single-threaded

--- server/app/src/main/kotlin/edu/kit/outwait/server/core/Server.kt
+++ server/app/src/main/kotlin/edu/kit/outwait/server/core/Server.kt
@@ -18,6 +18,8 @@ class Server {
         val config = Configuration();
         config.setHostname("127.0.0.1");
         config.setPort(8080);
+        config.setBossThreads(1);
+        config.setWorkerThreads(1);
 
         server = SocketIOServer(config);
 
@@ -31,7 +33,21 @@ class Server {
 
     fun run() {
         println("Hello World from server!")
-        server.start();
+
+        // DEBUG terminate the server after 10 seconds
+        java.util.Timer().schedule(object : java.util.TimerTask() {
+                        override fun run() {
+                            stop()
+                            System.exit(0)
+                        }
+                    }, 10000
+            )
+
+        server.start()
+    }
+
+    fun stop() {
+        server.stop()
     }
 }
 
