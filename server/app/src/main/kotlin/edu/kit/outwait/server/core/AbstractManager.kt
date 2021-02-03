1435754df9f6199b462ad8dd0db3e68edf4ce768
Erik Götzfried <erik.goetzfried@gmail.com>
2021-02-03 13:04:03 +0100

    Implement some server core classes

--- server/app/src/main/kotlin/edu/kit/outwait/server/core/AbstractManager.kt
+++ server/app/src/main/kotlin/edu/kit/outwait/server/core/AbstractManager.kt
@@ -2,6 +2,7 @@ package edu.kit.outwait.server.core
 
 import com.corundumstudio.socketio.SocketIOClient
 import com.corundumstudio.socketio.SocketIONamespace
+import com.corundumstudio.socketio.listener.ConnectListener;
 import edu.kit.outwait.server.protocol.Event
 import edu.kit.outwait.server.socketHelper.SocketAdapter
 import edu.kit.outwait.server.socketHelper.SocketFacade
@@ -12,11 +13,24 @@ abstract class AbstractManager(
 ) {
     protected val socketAdapter = SocketAdapter(namespace)
 
+    init {
+        configureConnectionCreation(namespace)
+    }
+
     abstract fun bindSocket(socket: SocketIOClient, socketFacade: SocketFacade)
 
-    private fun configureConnectionCreation() {
+    private fun configureConnectionCreation(namespace: SocketIONamespace) {
+        namespace.addConnectListener(
+            object : ConnectListener {
+                override fun onConnect(client: SocketIOClient) {
+                    val socketFacade = SocketFacade(client, socketAdapter);
+                    bindSocket(client, socketFacade);
+                }
+            }
+        );
     }
 
     fun configureEventListeners(events: List<Event>) {
+        socketAdapter.configureEvents(events)
     }
 }
--- server/app/src/main/kotlin/edu/kit/outwait/server/socketHelper/SocketFacade.kt
+++ server/app/src/main/kotlin/edu/kit/outwait/server/socketHelper/SocketFacade.kt
@@ -8,6 +8,10 @@ class SocketFacade(val socket: SocketIOClient, adapter: SocketAdapter) {
     internal val eventCallbacks = hashMapOf<Event, (receivedData: JSONObjectWrapper) -> Unit>()
     internal val disconnectCallback: () -> Unit = {}
 
+    init {
+        adapter.addFacadeForSocket(this, socket)
+    }
+
     fun send(event: Event, toSend: JSONObjectWrapper) {}
     fun onReceive(event: Event, callback: (receivedData: JSONObjectWrapper) -> Unit) {}
     fun onDisconnect(event: Event, callback: () -> Unit) {}
