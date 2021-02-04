3965d936ebc0f8edb555a16c68cafb836e31f534
Erik Götzfried <erik.goetzfried@gmail.com>
2021-02-04 10:53:35 +0100

    Implement socket helper package

--- server/app/src/main/kotlin/edu/kit/outwait/server/socketHelper/SocketAdapter.kt
+++ server/app/src/main/kotlin/edu/kit/outwait/server/socketHelper/SocketAdapter.kt
@@ -1,13 +1,57 @@
 package edu.kit.outwait.server.socketHelper
 
+import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.SocketIOClient
 import com.corundumstudio.socketio.SocketIONamespace
+import com.corundumstudio.socketio.listener.DataListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
 import edu.kit.outwait.server.protocol.Event
 
-class SocketAdapter(namespace: SocketIONamespace) {
+class SocketAdapter(val namespace: SocketIONamespace) {
     private val facades = hashMapOf<SocketIOClient, SocketFacade>()
 
-    fun configureEvents(events: List<Event>) {}
-    fun addFacadeForSocket(facade: SocketFacade, socket: SocketIOClient) {}
-    private fun removeFacade(facade: SocketFacade) {}
+    fun configureEvents(events: List<Event>) {
+        // Configure all event listeners
+        for (e in events) {
+            namespace.addEventListener(
+                e.getEventTag(),
+                String::class.java,
+                object : DataListener<String> {
+                    override fun onData(
+                        client: SocketIOClient,
+                        dat:String,
+                        ackRequest: AckRequest
+                    ) {
+                        val jsonWrapper = e.createWrapper(dat)
+                        val facade = facades[client]
+                        if (facade != null) {
+                            val callback = facade.eventCallbacks[e]
+                            if (callback != null) {
+                                callback(jsonWrapper)
+                            }
+                        }
+                    }
+                }
+            )
+        }
+
+        // Configure disconnect listener
+        namespace.addDisconnectListener(
+            object : DisconnectListener {
+                override fun onDisconnect(client: SocketIOClient) {
+                    val facade = facades[client]
+                    if (facade != null) {
+                        facade.disconnectCallback()
+                    }
+                }
+            }
+        );
+    }
+
+    fun addFacadeForSocket(facade: SocketFacade, socket: SocketIOClient) {
+        facades.put(socket, facade)
+    }
+    private fun removeFacade(facade: SocketFacade) {
+        facades.values.remove(facade)
+    }
 }
