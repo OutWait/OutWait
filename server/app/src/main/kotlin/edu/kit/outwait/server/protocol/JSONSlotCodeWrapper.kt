14e15a410b4936769a946afefe3ff8247d2b43a1
Erik Götzfried <erik.goetzfried@gmail.com>
2021-02-04 10:51:54 +0100

    Minor fixes with json wrapper
    Typo fixed and unnecessary abstract removed.

--- server/app/src/main/kotlin/edu/kit/outwait/server/protocol/JSONSlotCodeWrapper.kt
+++ server/app/src/main/kotlin/edu/kit/outwait/server/protocol/JSONSlotCodeWrapper.kt
@@ -3,7 +3,7 @@ package edu.kit.outwait.server.protocol
 import edu.kit.outwait.server.slot.SlotCode
 import org.json.JSONObject
 
-abstract class JSONSlotCodeWrapper : JSONObjectWrapper {
+class JSONSlotCodeWrapper : JSONObjectWrapper {
     constructor() {}
     constructor(obj: JSONObject) {}
     fun setSlotCode (slotCode: SlotCode) {}
