b345b95460d61a75fd3206986c4409f48640866a
Benjamin Hänsel <benjamin.haensel@benjaminhaensel.de>
2021-02-13 12:31:03 +0100

    Add Pauses in Gravity Algorithm

--- app/OutWait/app/src/main/java/elite/kit/outwait/waitingQueue/gravityQueue/GravityQueueConverter.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/waitingQueue/gravityQueue/GravityQueueConverter.kt
@@ -3,11 +3,13 @@ package elite.kit.outwait.waitingQueue.gravityQueue
 import elite.kit.outwait.waitingQueue.timeSlotModel.Pause
 import elite.kit.outwait.waitingQueue.timeSlotModel.TimeSlot
 import org.joda.time.DateTime
+import org.joda.time.Duration
 import org.joda.time.Interval
 
 class GravityQueueConverter {
     fun gravitySlotListToTimeSlotList(gravitySlotList : List<ClientGravitySlot>, auxiliaryIdentifiers : HashMap<String, String>, currentSlotStartedTime: DateTime, now: DateTime) : List <TimeSlot>{
-        val timeSlotList = listOf<TimeSlot>()
+
+
 
         //Verspätet sich der aktuelle Slot? -> Dummy anpassen
         var firstTimeSlotDummy = Pause(Interval(currentSlotStartedTime, currentSlotStartedTime))
@@ -15,23 +17,27 @@ class GravityQueueConverter {
             firstTimeSlotDummy = Pause(Interval(now - gravitySlotList[1].duration, now - gravitySlotList[1].duration))
         }
 
+        val timeSlotList = mutableListOf<TimeSlot>(firstTimeSlotDummy)
+
         //Gravity-Algorithmus
         for (slot in gravitySlotList){
-            timeSlotList.plus(slot.toClientTimeSlot(timeSlotList.last()))
+            timeSlotList.add(slot.toClientTimeSlot(timeSlotList.last()))
         }
 
         //Dummy-Slot entfernen
-        timeSlotList.minus(firstTimeSlotDummy)
+        timeSlotList.remove(firstTimeSlotDummy)
 
-        val listWithPauses = listOf<TimeSlot>()
-        /*
-        for (i in 0..timeSlotList.lastIndex){
-            listWithPauses.plus(timeSlotList[i])
-            if(i != timeSlotList.lastIndex && ){
-
+        //Pausen zwischen auseinanderliegenden Slots einfügen
+        val listWithPauses = mutableListOf<TimeSlot>()
+        var i : Int = 0
+        while(i < timeSlotList.size){
+            listWithPauses.add(timeSlotList[i])
+            if(i != timeSlotList.lastIndex && timeSlotList[i].interval.end < timeSlotList[i+1].interval.start){
+                timeSlotList.add(i+1, Pause(Interval(timeSlotList[i].interval.end, timeSlotList[i+1].interval.start)))
             }
-        }*/
+            i++
+        }
 
-        return timeSlotList
+        return listWithPauses
     }
 }
