3b24f614b225ff4c460332ed14d040ae4a33a9a5
ensarevlideveloper <ensarevli@gmail.com>
2021-02-15 15:08:17 +0100

    Minor comment correction in Client.kt, Implementations and Corrections in DatabaseWrapper.kt
    Corrected Queries in DatabaseWrapper.kt. Implemented registerReceiver, saveManagementSettings, checkIfSlotsExists(new), getSlotManagementInformation (new). Furthermore implemented UpdateMediator methods in DatabaseWrapper.kt

--- server/app/src/main/kotlin/edu/kit/outwait/server/protocol/JSONSlotDataWrapper.kt
+++ server/app/src/main/kotlin/edu/kit/outwait/server/protocol/JSONSlotDataWrapper.kt
@@ -2,15 +2,15 @@ package edu.kit.outwait.server.protocol
 
 import edu.kit.outwait.server.management.ManagementDetails
 import edu.kit.outwait.server.management.SlotManagementInformation
-import java.time.Duration
-import java.util.Date
 import org.json.JSONObject
+import java.time.Duration
+import java.util.*
 
-class JSONSlotDataWrapper(obj: JSONObject) : JSONSlotCodeWrapper(obj) {
+class JSONSlotDataWrapper (obj: JSONObject) : JSONSlotCodeWrapper(obj){
     constructor() : this(JSONObject())
     fun setSlotApprox(slotApprox: Date) {}
-    fun setInformation(information: SlotManagementInformation) {}
     fun getSlotApprox(): Date { return Date(0) }
+    fun setInformation(information: SlotManagementInformation) {}
     fun getInformation(): SlotManagementInformation {
         return SlotManagementInformation(ManagementDetails(""), Duration.ZERO, Duration.ZERO)
     }
