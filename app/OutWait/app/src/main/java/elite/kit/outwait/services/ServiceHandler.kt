64d6d42d7a1d2b4f3f609ec854ffd278d811351c
Dennis Stroh <58657479+de-stro@users.noreply.github.com>
2021-02-19 17:30:14 +0100

    Add stub for method that delegates the call to start the TimerService

--- app/OutWait/app/src/main/java/elite/kit/outwait/services/ServiceHandler.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/services/ServiceHandler.kt
@@ -1,10 +1,15 @@
 package elite.kit.outwait.services
 
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.content.ContextCompat
+
 class ServiceHandler {
 
-
-
-    fun startTimerService() {
-        
+    fun startTimerService(context: Context) {
+        val serviceIntent = Intent(context, TimerService::class.java)
+        ContextCompat.startForegroundService(context, serviceIntent)
+        Log.i("ServiceHandler", "We returned from startOnCommand")
     }
 }
