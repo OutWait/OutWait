c17e08b25de77c6ea0177f47959e1c86c9c95ee9
Benjamin Hänsel <benjamin.haensel@benjaminhaensel.de>
2021-02-20 20:37:57 +0100

    Access approx Slot Time in ViewModel

--- app/OutWait/app/src/main/java/elite/kit/outwait/clientScreens/remainingTimeScreen/RemainingTimeViewModel.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/clientScreens/remainingTimeScreen/RemainingTimeViewModel.kt
@@ -1,5 +1,7 @@
 package elite.kit.outwait.clientScreens.remainingTimeScreen
 
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,6 +10,7 @@ import elite.kit.outwait.clientDatabase.ClientInfo
 import elite.kit.outwait.clientRepository.ClientRepository
 import elite.kit.outwait.instituteRepository.InstituteRepository
 import org.joda.time.DateTime
+import org.joda.time.Duration
 import org.joda.time.Interval
 import javax.inject.Inject
 
@@ -22,8 +25,28 @@ class RemainingTimeViewModel  @Inject constructor(private val repo : ClientRepos
     //TODO calculation of remaining time
     var clientInfo = MutableLiveData<ClientInfo>()
 
+    private val _remainingMinutes = MutableLiveData<Int>()
+    val remainingMinutes
+        get() = _remainingMinutes as LiveData<Int>
+
 init {
     //clientInfo=ClientRepository.getActiveSlots.value[0]
+    val slots = repo.getActiveSlots().value
+    if (slots !== null && slots.isNotEmpty()){
+        val approxTime = slots.last().approximatedTime
+        val timeLeft = Duration(approxTime.millis - DateTime.now().millis)
+        _remainingMinutes.value = timeLeft.standardMinutes.toInt()
+        Log.d("RemTimeVM", "${remainingMinutes.value} minuten verbleibend")
+    }
+
+    repo.getActiveSlots().observeForever {
+        if (it.isNotEmpty()){
+            val approxTime = it.last().approximatedTime
+            val timeLeft = Duration(approxTime.millis - DateTime.now().millis)
+            _remainingMinutes.value = timeLeft.standardMinutes.toInt()
+            Log.d("RemTimeVM", "${remainingMinutes.value} minuten verbleibend")
+        }
+    }
 }
     fun navigateBack() {
         TODO("Not yet implemented")
