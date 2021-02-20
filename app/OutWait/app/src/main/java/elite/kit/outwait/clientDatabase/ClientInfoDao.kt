284bab84a60591e3698b1ced3cd0aabdadc9789a
Dennis Stroh <58657479+de-stro@users.noreply.github.com>
2021-02-20 17:06:47 +0100

    Make return of clientDB getter methods of type nullable

--- app/OutWait/app/src/main/java/elite/kit/outwait/clientDatabase/ClientInfoDao.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/clientDatabase/ClientInfoDao.kt
@@ -13,19 +13,19 @@ interface ClientInfoDao {
     fun update(clientInfo: ClientInfo)
 
     @Query("Select * From client_table Where :waitCode")
-    fun getClientInfo(waitCode: String): ClientInfo
+    fun getClientInfo(waitCode: String): ClientInfo?
 
     @Delete
     fun deleteClientInfo(info: ClientInfo)
 
     @Query("Select * From client_table Where :slotCode")
-    fun getClientInfoObservable(slotCode: String): LiveData<ClientInfo>
+    fun getClientInfoObservable(slotCode: String): LiveData<ClientInfo?>
 
     @Query("Select * From client_table")
-    fun getAllClientInfo(): List<ClientInfo>
+    fun getAllClientInfo(): List<ClientInfo?>
 
     @Query("Select * From client_table")
-    fun getAllClientInfoObservable(): LiveData<List<ClientInfo>>
+    fun getAllClientInfoObservable(): LiveData<List<ClientInfo?>>
 
     @Query("DELETE FROM client_table")
     fun clearTable()
