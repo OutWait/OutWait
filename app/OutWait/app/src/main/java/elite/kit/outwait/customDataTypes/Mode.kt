2c54d29eb97817da7ca7038e7af8f3d4f731a0bb
Dennis Stroh <58657479+de-stro@users.noreply.github.com>
2021-02-26 10:52:40 +0100

    Add modeName attribute and override toString() of enum Mode, adapt the parsing of received ManagementSettings in JSONWrapper

--- app/OutWait/app/src/main/java/elite/kit/outwait/customDataTypes/Mode.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/customDataTypes/Mode.kt
@@ -3,6 +3,10 @@ package elite.kit.outwait.customDataTypes
 /**
  * currently our app supports two modes. See specification document K1
  */
-enum class Mode {
-    ONE, TWO
+enum class Mode(private val modeName: String) {
+    ONE("one"), TWO("two");
+
+    override fun toString(): String {
+        return this.modeName
+    }
 }
