7beb5f7e616f5899246fae3591b5003356af1048
MuecahidAbdullahYeniguen <m-a-yeniguen@hotmail.com>
2021-02-27 20:26:22 +0100

    clean screens of rv

--- app/OutWait/app/src/main/java/elite/kit/outwait/dataItem/DataItem.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/dataItem/DataItem.kt
@@ -1,7 +1,8 @@
 package elite.kit.outwait.dataItem
 
 import elite.kit.outwait.waitingQueue.timeSlotModel.TimeSlot
+import elite.kit.outwait.waitingQueue.timeSlotModel.Type
 
 abstract class DataItem() {
-
+abstract fun getType(): Type
 }
--- app/OutWait/app/src/main/java/elite/kit/outwait/dataItem/TimeSlotItem.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/dataItem/TimeSlotItem.kt
@@ -1,7 +1,11 @@
 package elite.kit.outwait.dataItem
 
 import elite.kit.outwait.waitingQueue.timeSlotModel.TimeSlot
+import elite.kit.outwait.waitingQueue.timeSlotModel.Type
 
 class TimeSlotItem(private var timeSlot:TimeSlot): DataItem() {
+    override fun getType(): Type {
+        return timeSlot.getType()
+    }
 
 }
