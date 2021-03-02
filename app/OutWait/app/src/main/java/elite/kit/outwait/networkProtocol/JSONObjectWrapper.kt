cf6ec4d8f073f6f9511b7736ee99dd6aec0b5033
Dennis Stroh <58657479+de-stro@users.noreply.github.com>
2021-03-02 10:54:45 +0100

    Move constants of network protocol package in dedicated constants file

--- app/OutWait/app/src/main/java/elite/kit/outwait/networkProtocol/JSONObjectWrapper.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/networkProtocol/JSONObjectWrapper.kt
@@ -2,35 +2,6 @@ package elite.kit.outwait.networkProtocol
 
 import org.json.JSONObject
 
-const val SLOT_CODE = "slotCode"
-const val USERNAME = "username"
-const val PASSWORD= "password"
-
-const val DURATION= "duration"
-const val TIME_OF_CREATION = "timeOfCreation"
-const val APPOINTMENT_TIME = "appointmentTime"
-const val MOVED_SLOT = "movedSlot"
-const val OTHER_SLOT = "otherSlot"
-
-const val NEW_TIME = "newTime"
-const val NEW_DURATION = "newDuration"
-
-const val PRIORITIZATION_TIME = "prioritizationTime"
-const val MODE = "mode"
-const val DELAY_NOTIFICATION_TIME = "delayNotificationTime"
-const val NOTIFICATION_TIME = "notificationTime"
-const val DEFAULT_SLOT_DURATION = "defaultSlotDuration"
-
-const val APPROX_TIME = "approxTime"
-const val INSTITUTE_NAME = "instituteName"
-
-const val ERROR_MESSAGE = "errorMessage"
-
-const val CURRENT_SLOT_STARTED_TIME = "currentSlotStartedTime"
-const val SLOT_ORDER = "slotOrder"
-const val SPONTANEOUS_SLOTS = "spontaneousSlots"
-const val FIXED_SLOTS = "fixedSlots"
-
 /**
  * This is the base class for the JSONObjectWrapper
  *
