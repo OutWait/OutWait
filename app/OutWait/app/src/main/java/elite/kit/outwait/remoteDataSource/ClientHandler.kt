223703f8ea7025b14edb27f361dbb71db0013360
Dennis Stroh <58657479+de-stro@users.noreply.github.com>
2021-03-03 23:44:57 +0100

    Add comments

--- app/OutWait/app/src/main/java/elite/kit/outwait/remoteDataSource/ClientHandler.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/remoteDataSource/ClientHandler.kt
@@ -3,9 +3,12 @@ package elite.kit.outwait.remoteDataSource
 import androidx.lifecycle.LiveData
 
 /**
- * This interface describes all methods for network communication, that the
+ * This interface "ClientHandler" represents the "abstract product"
+ * in the, here used and commonly known as, "abstract factory pattern".
+ * It describes all methods for network communication, that the
  * client repository (or higher tier) can use to send to and receive data from the server.
- * They must be provided by the respective implementation of the network communication
+ * They must be provided by the "concrete product" of this pattern, meaning the
+ * respective implementation of the network communication, implementing this interface
  *
  */
 interface ClientHandler {
@@ -19,7 +22,7 @@ interface ClientHandler {
     fun initCommunication(): Boolean
 
     /**
-     * This method releases all communication resources, depending on the used
+     * This abstract method releases all communication resources, depending on the used
      * implementation of client-server communication (e.g WebSockets, REST etc)
      *
      * @return true, if communication was successfully released, else false
@@ -27,25 +30,25 @@ interface ClientHandler {
     fun endCommunication(): Boolean
 
     /**
-     * This method emits the event "listenSlot@S" with the given slotCode as data to the server
+     * This abstract method emits the event "listenSlot@S" with the given slotCode as data to the server
      *
      * @param slotCode specifies the slot that the client wants to observe
      */
     fun newCodeEntered(slotCode: String)
 
     /**
-     * This method emits the event "refreshSlotApprox@S" with the given slotCode as data
+     * This abstract method emits the event "refreshSlotApprox@S" with the given slotCode as data
      *
      * @param slotCode specifies the slot for which the waiting time refresh was requested
      */
     fun refreshWaitingTime(slotCode: String)
 
     /**
-     * Getter for LiveData, that provides the client repository (or higher tier)
+     * Abstract getter for LiveData, that provides the client repository (or higher tier)
      * with error messages, that may be time-displaced or unexpectedly received by
      * the server
      *
-     * @return list of ClientServerErrors, as LiveData
+     * @return list of type ClientServerErrors, as LiveData
      */
     fun getErrors() : LiveData<List<ClientServerErrors>>
 }
