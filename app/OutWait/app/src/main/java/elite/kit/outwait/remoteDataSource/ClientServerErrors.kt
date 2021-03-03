65862e6a2b3b8dee61ad950e57a988ce3e825f4e
Dennis Stroh <58657479+de-stro@users.noreply.github.com>
2021-03-03 22:24:06 +0100

    Add comments

--- app/OutWait/app/src/main/java/elite/kit/outwait/remoteDataSource/ClientServerErrors.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/remoteDataSource/ClientServerErrors.kt
@@ -1,5 +1,11 @@
 package elite.kit.outwait.remoteDataSource
 
+/**
+ * This enumeration represents error messages in the client-server communication,
+ * that the client repository (or higher tier) should handle (eg. so the
+ * app-user can get notified in an useful way)
+ * //TODO Noch was zu den einzelnen Errors sagen?
+ */
 enum class ClientServerErrors {
     INVALID_SLOT_CODE,
     INVALID_REQUEST
--- app/OutWait/app/src/main/java/elite/kit/outwait/remoteDataSource/ManagementServerErrors.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/remoteDataSource/ManagementServerErrors.kt
@@ -1,5 +1,11 @@
 package elite.kit.outwait.remoteDataSource
 
+/**
+ * This enumeration represents error messages in the management-server communication,
+ * that the institute repository (or higher tier) should handle (eg. so the
+ * app-user can get notified in an useful way)
+ * //TODO Noch was zu den einzelnen Errors sagen?
+ */
 enum class ManagementServerErrors {
     TRANSACTION_DENIED,
     LOGIN_DENIED,
