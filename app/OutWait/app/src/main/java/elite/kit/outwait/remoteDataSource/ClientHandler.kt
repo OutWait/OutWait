dda252a8506aed54a15016f0836c159456dc9aab
Dennis Stroh <58657479+de-stro@users.noreply.github.com>
2021-03-03 18:54:43 +0100

    Add comments

--- app/OutWait/app/src/main/java/elite/kit/outwait/remoteDataSource/ClientHandler.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/remoteDataSource/ClientHandler.kt
@@ -2,24 +2,50 @@ package elite.kit.outwait.remoteDataSource
 
 import androidx.lifecycle.LiveData
 
+/**
+ * This interface describes all methods for network communication, that the
+ * client repository (or higher tier) can use to send to and receive data from the server.
+ * They must be provided by the respective implementation of the network communication
+ *
+ */
 interface ClientHandler {
 
-    // Methoden um Verbindung zum Server aufzubauen/abzubauen
-    // Es müssen Referenzen auf den Handler bzw. das remoteDataSourcePaket bzw den Socket
-    // gezählt werden, erst wenn niemand bzw. nur einer eine Ref. hält, darf Komm. plattgemacht werden
+    /**
+     * This method sets up the communication resources, depending on the used
+     * implementation of client-server communication (e.g WebSockets, REST etc)
+     *
+     * @return true, if communication was successfully established, else false
+     */
     fun initCommunication(): Boolean
 
+    /**
+     * This method releases all communication resources, depending on the used
+     * implementation of client-server communication (e.g WebSockets, REST etc)
+     *
+     * @return true, if communication was successfully released, else false
+     */
     fun endCommunication(): Boolean
 
-    /*
- Getter für LiveData um Events zu signalisieren? Eingeloggt, Ausgeloggt usw.
- //TODO Was wird hier gebraucht? Am besten mit LiveData?
- //Wie im AndroidKurs muss man dann auch sicherstellen, dass onEventFinish() aufgerufen wird!!
-  */
-
+    /**
+     * This method emits the event "listenSlot@S" with the given slotCode as data to the server
+     *
+     * @param slotCode specifies the slot that the client wants to observe
+     */
     fun newCodeEntered(slotCode: String)
 
+    /**
+     * This method emits the event "refreshSlotApprox@S" with the given slotCode as data
+     *
+     * @param slotCode specifies the slot for which the waiting time refresh was requested
+     */
     fun refreshWaitingTime(slotCode: String)
 
+    /**
+     * Getter for LiveData, that provides the client repository (or higher tier)
+     * with error messages, that may be time-displaced or unexpectedly received by
+     * the server
+     *
+     * @return list of ClientServerErrors, as LiveData
+     */
     fun getErrors() : LiveData<List<ClientServerErrors>>
 }
