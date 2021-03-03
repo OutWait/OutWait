2588efeb765d1348c1b9300608d0dfa05108e4b1
Dennis Stroh <58657479+de-stro@users.noreply.github.com>
2021-03-03 18:55:16 +0100

    Add constants for service package

---
+++ app/OutWait/app/src/main/java/elite/kit/outwait/services/ServiceConstants.kt
@@ -0,0 +1,19 @@
+package elite.kit.outwait.services
+
+const val PERM_CHANNEL_ID = "permChannel_ID"
+const val PERM_CHANNEL_NAME = "Permanent_Info_Channel"
+const val PERM_CHANNEL_DESCRIPTION = "Displays information regarding next appointment"
+
+const val SECOND_CHANNEL_ID = "secondChannel_ID"
+const val SECOND_CHANNEL_NAME = "Second_Info_Channel"
+const val SECOND_CHANNEL_DESCRIPTION = "Displays info and sudden changes regarding all appointments"
+
+const val PERM_NOTIFICATION_ID = 123
+const val DELAY_NOTIFICATION_ID = 456
+const val PENDING_NOTIFICATION_ID = 789
+
+//TODO Die Default texte des injecteten Notif-Builder in xml resources packen?
+//Geht das mit der Injection zusammen?
+const val PERM_CHANNEL_DEFAULT_TITLE = "Your next appointment"
+const val SECOND_CHANNEL_DEFAULT_TITLE = "Info regarding appointments"
+const val NOTIFICATION_CHANNEL_DEFAULT_TEXT = "will be displayed here"
