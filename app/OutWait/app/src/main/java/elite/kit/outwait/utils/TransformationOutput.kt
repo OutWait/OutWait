5879525a3308904fd735dcbc98ccd44b0b648065
Benjamin Hänsel <benjamin.haensel@benjaminhaensel.de>
2021-03-03 11:50:01 +0100

    switch to period formatter in TransformationOutput

--- app/OutWait/app/src/main/java/elite/kit/outwait/utils/TransformationOutput.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/utils/TransformationOutput.kt
@@ -5,23 +5,31 @@ import org.joda.time.Duration
 import org.joda.time.Interval
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.format.DateTimeFormatter
+import org.joda.time.format.PeriodFormatter
+import org.joda.time.format.PeriodFormatterBuilder
 
 
 object TransformationOutput {
 
     private val formatter: DateTimeFormatter = DateTimeFormat.forPattern("HH:mm")
-
+    private val periodFormatter = PeriodFormatterBuilder()
+        .minimumPrintedDigits(2)
+        .printZeroAlways()
+        .appendHours()
+        .appendLiteral(":")
+        .printZeroAlways()
+        .appendMinutes()
+        .toFormatter()
 
     fun appointmentToString(appointmentTime: DateTime):String{
         return formatter.print(appointmentTime)
     }
 
     fun intervalToString(interval: Interval):String{
-        return  formatter.print(interval.toDurationMillis())
+        return periodFormatter.print(interval.toPeriod())
     }
 
     fun durationToString(duration: Duration): String{
-        //return formatter.print(duration.millis)
-        return "" + duration.standardHours + ":" + duration.standardMinutes
+        return periodFormatter.print(duration.toPeriod())
     }
 }
