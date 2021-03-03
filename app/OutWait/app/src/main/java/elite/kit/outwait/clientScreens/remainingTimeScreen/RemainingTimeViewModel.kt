6e192906a2451c1ea70e07413a37973b4a4ca0ef
MuecahidAbdullahYeniguen <m-a-yeniguen@hotmail.com>
2021-03-03 09:12:35 +0100

    Update remainingTimeVM
    New type of livedata and binding with new gui

--- app/OutWait/app/src/main/java/elite/kit/outwait/clientScreens/remainingTimeScreen/RemainingTimeViewModel.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/clientScreens/remainingTimeScreen/RemainingTimeViewModel.kt
@@ -9,10 +9,12 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import elite.kit.outwait.clientDatabase.ClientInfo
 import elite.kit.outwait.clientRepository.ClientRepository
 import elite.kit.outwait.instituteRepository.InstituteRepository
+import elite.kit.outwait.utils.TransformationOutput
 import org.joda.time.DateTime
 import org.joda.time.Duration
 import org.joda.time.Interval
 import javax.inject.Inject
+import kotlin.time.milliseconds
 
 @HiltViewModel
 class RemainingTimeViewModel  @Inject constructor(private val repo : ClientRepository): ViewModel() {
@@ -25,26 +27,30 @@ class RemainingTimeViewModel  @Inject constructor(private val repo : ClientRepos
     //TODO calculation of remaining time
     var clientInfo = MutableLiveData<ClientInfo>()
     var clientInfoList = repo.getActiveSlots()
-    private val _remainingMinutes = MutableLiveData<Long>()
-    val remainingMinutes
-        get() = _remainingMinutes as LiveData<Long>
+    private val _remainingTime = MutableLiveData<String>()
+    val remainingTime
+        get() = _remainingTime as LiveData<String>
 
 init {
     //clientInfo=ClientRepository.getActiveSlots.value[0]
+
+    //Ausgabe 20:13 also HH:mm
+    TransformationOutput.intervalToString(Interval(_remainingTime.value!!))
+
     val slots = repo.getActiveSlots().value
     if (slots !== null && slots.isNotEmpty()){
         val approxTime = slots.last().approximatedTime
         val timeLeft = Duration(approxTime.millis - DateTime.now().millis)
-        _remainingMinutes.value = timeLeft.millis
-        Log.d("RemTimeVM", "${remainingMinutes.value} minuten verbleibend")
+        _remainingTime.value = TransformationOutput.intervalToString(Interval(timeLeft.millis))
+        Log.d("RemTimeVM", "${_remainingTime.value} minuten verbleibend")
     }
 
     repo.getActiveSlots().observeForever {
         if (it.isNotEmpty()){
             val approxTime = it.last().approximatedTime
             val timeLeft = Duration(approxTime.millis - DateTime.now().millis)
-            _remainingMinutes.value = timeLeft.millis
-            Log.d("RemTimeVM", "${remainingMinutes.value} minuten verbleibend")
+            _remainingTime.value = TransformationOutput.intervalToString(Interval(timeLeft.millis))
+            Log.d("RemTimeVM", "${_remainingTime.value} minuten verbleibend")
         }
     }
 }
