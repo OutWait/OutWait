a7dcbd0c5b83761423370becaf4ce9f0fd0615af
Benjamin Hänsel <benjamin.haensel@benjaminhaensel.de>
2021-03-04 13:09:27 +0100

    Comment and clean up client repo, set serviceStarted false when db empty

--- app/OutWait/app/src/main/java/elite/kit/outwait/clientRepository/ClientErrors.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/clientRepository/ClientErrors.kt
@@ -2,7 +2,12 @@ package elite.kit.outwait.clientRepository
 
 import elite.kit.outwait.R
 
+/**
+ * Collection of errors that the client repo can use to inform others
+ * (especially the GUI with LiveData) about errors which happen
+ *
+ * @property message String resource ID where the error message can be found.
+ */
 enum class ClientErrors(val message: Int) {
-    //EXAMPLE_ERROR(R.string.EXAMPLE_ERROR)
     INVALID_SLOT_CODE(R.string.INVALID_SLOT_CODE)
 }
