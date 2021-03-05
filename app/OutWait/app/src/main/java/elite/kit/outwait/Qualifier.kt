402137bb27be592ac79aef1ff20647dea78046b6
Dennis Stroh <58657479+de-stro@users.noreply.github.com>
2021-03-05 17:38:24 +0100

    Add comments in service package

--- app/OutWait/app/src/main/java/elite/kit/outwait/Qualifier.kt
+++ app/OutWait/app/src/main/java/elite/kit/outwait/Qualifier.kt
@@ -2,10 +2,18 @@ package elite.kit.outwait
 
 import javax.inject.Qualifier
 
+/**
+ * Qualifier, specifying the injection of the first notification builder of type NotificationCompat.Builder
+ *
+ */
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class channel_1NotificationBuilder
 
+/**
+ * Qualifier, specifying the injection of the second notification builder of type NotificationCompat.Builder
+ *
+ */
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class channel_2NotificationBuilder
