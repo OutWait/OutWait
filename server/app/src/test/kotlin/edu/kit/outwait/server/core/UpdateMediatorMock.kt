a52c05bd8df5351795a1b5ca396c663bd8b44452
Erik Götzfried <erik.goetzfried@gmail.com>
2021-03-05 16:10:51 +0100

    Formatting

--- server/app/src/test/kotlin/edu/kit/outwait/server/core/UpdateMediatorMock.kt
+++ server/app/src/test/kotlin/edu/kit/outwait/server/core/UpdateMediatorMock.kt
@@ -1,16 +1,15 @@
 package edu.kit.outwait.server.core
 
 import edu.kit.outwait.server.client.SlotInformationReceiver
-import edu.kit.outwait.server.slot.Slot
 
 class UpdateMediatorMock {
     val receivers = mutableListOf<SlotInformationReceiver>()
 
-    fun subscribeReceiver(slot: SlotInformationReceiver){
+    fun subscribeReceiver(slot: SlotInformationReceiver) {
         receivers.add(slot)
     }
 
-    fun unsubscribeReceiver(slot: SlotInformationReceiver){
+    fun unsubscribeReceiver(slot: SlotInformationReceiver) {
         receivers.remove(slot)
     }
 }
