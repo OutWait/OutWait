9b815e648083aee9d39ad37057eb93c8c9468cc5
Benjamin Hänsel <benjamin.haensel@benjaminhaensel.de>
2021-03-15 12:16:40 +0100

    Unit Test fixed gravity slot class

---
+++ app/OutWait/app/src/test/java/elite/kit/outwait/waitingQueue/gravityQueue/SpontaneousGravitySlotTest.kt
@@ -0,0 +1,17 @@
+package elite.kit.outwait.waitingQueue.gravityQueue
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class SpontaneousGravitySlotTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
