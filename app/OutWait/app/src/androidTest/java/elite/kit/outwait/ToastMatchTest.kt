24be015ea5a717cc7cc5a4f69d7a967af9f50bfd
MuecahidAbdullahYeniguen <m-a-yeniguen@hotmail.com>
2021-03-18 12:48:09 +0100

    Fix bugs with toastmatcher, it works

--- app/OutWait/app/src/androidTest/java/elite/kit/outwait/ToastMatchTest.kt
+++ app/OutWait/app/src/androidTest/java/elite/kit/outwait/ToastMatchTest.kt
@@ -2,19 +2,24 @@ package elite.kit.outwait
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.activityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import elite.kit.outwait.util.ToastMatcher
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
 
-@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class ToastMatchTest {
-    @get:Rule
+
+    @get:Rule(order = 0)
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
     var openActivityRule = activityScenarioRule<MainActivity>()
 
     @Test
@@ -23,8 +28,9 @@ class ToastMatchTest {
 
         onView(withId(R.id.btnLoginFrag)).perform(click())
        // Thread.sleep(3000)
-        onView(withText("Login denied!")).inRoot(ToastMatcher())
-            .check(matches(isDisplayed()))
+        onView(withText("Anmeldung fehlgeschlagen!")).inRoot(ToastMatcher()).check(matches(isDisplayed()))
+
+
         openActivityRule.scenario.close()
     }
 }
--- app/OutWait/app/src/androidTest/java/elite/kit/outwait/util/ToastMatcher.kt
+++ app/OutWait/app/src/androidTest/java/elite/kit/outwait/util/ToastMatcher.kt
@@ -11,10 +11,10 @@ class ToastMatcher: TypeSafeMatcher<Root?>(){
 
 
     override fun matchesSafely(item: Root?): Boolean {
-        val type: Int? = item?.getWindowLayoutParams()?.get()?.type
+        val type: Int? = item?.windowLayoutParams?.get()?.type
         if (type == WindowManager.LayoutParams.TYPE_TOAST) {
-            val windowToken: IBinder = item.getDecorView().getWindowToken()
-            val appToken: IBinder = item.getDecorView().getApplicationWindowToken()
+            val windowToken: IBinder = item.decorView.windowToken
+            val appToken: IBinder = item.decorView.applicationWindowToken
             if (windowToken === appToken) { // means this window isn't contained by any other windows.
                 return true
             }
