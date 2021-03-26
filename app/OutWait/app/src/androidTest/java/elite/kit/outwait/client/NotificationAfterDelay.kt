b5f96eec4a780c4afe5780b23c3243e1d96f1e58
Dennis Stroh <58657479+de-stro@users.noreply.github.com>
2021-03-26 14:57:57 +0100

    Polish NotificationAfterDelay.kt Test for T15

--- app/OutWait/app/src/androidTest/java/elite/kit/outwait/client/NotificationAfterDelay.kt
+++ app/OutWait/app/src/androidTest/java/elite/kit/outwait/client/NotificationAfterDelay.kt
@@ -1,12 +1,10 @@
 package elite.kit.outwait.client
 
 import android.content.Context
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -151,36 +149,7 @@ class NotificationAfterDelay {
      */
     @After
     fun cleanUp() {
-        /*
-        // perform login
-        instituteRepo.login(
-            VALID_TEST_USERNAME,
-            VALID_TEST_PASSWORD
-        )
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
 
-        // check that we are logged in
-        assert(instituteRepo.isLoggedIn().value!!)
-
-        // clean up waiting queue (on server side also)
-        val timeSlots = instituteRepo.getObservableTimeSlotList().value
-
-
-        if (timeSlots != null && timeSlots.isNotEmpty()) {
-            val onlyClientSlots : List<ClientTimeSlot> = timeSlots.filterIsInstance<ClientTimeSlot>()
-            for (ClientTimeSlot in onlyClientSlots){
-                // delete slot with retrieved slotCode from waiting queue
-                instituteRepo.deleteSlot(ClientTimeSlot.slotCode)
-                Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-            }
-            // save the transaction and the changes made
-            CoroutineScope(Dispatchers.Main).launch {
-                instituteRepo.saveTransaction()
-            }
-            Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-        }
-
-         */
         // logout of management
         CoroutineScope(Dispatchers.Main).launch {
             instituteRepo.logout()
@@ -326,163 +295,10 @@ class NotificationAfterDelay {
         }
         Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
 
-        // check the result and assert expected result
-        // notify for delay notification was called (once)
+        // check the result and assert expected result is met
+        // notify for delay notification was called (exactly once) on the mock
         verify (exactly = 1)
         { injectedManager.notify(DELAY_NOTIFICATION_ID, any()) }
-
-        /*
-        /*
-        // logout of management
-        CoroutineScope(Dispatchers.Main).launch {
-            instituteRepo.logout()
-        }
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-
-        // check that we are logged out
-        assert(!instituteRepo.isLoggedIn().value!!)
-
-         */
-
-
-
-        /*
-        // second device simulieren?!
-        val newPrefs = Preferences(Duration(DEFAULT_DURATION_MILLIS), Duration(DEFAULT_DURATION_MILLIS),
-        Duration(THIRTY_MINUTE_DURATION_MILLIS), Duration(DEFAULT_DURATION_MILLIS), Mode.ONE)
-        instituteRepo.changePreferences(newPrefs)
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-
-        // perform action 4 //TODO mit digit selector als gui eingabe machen
-        instituteRepo.newSpontaneousSlot(FIRST_SLOT_IDENTIFIER, Duration(TEN_MINUTE_DURATION_MILLIS))
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-
-        // perform action 5 //TODO mit digit selector als gui eingabe machen
-        instituteRepo.newSpontaneousSlot(SECOND_SLOT_IDENTIFIER, Duration(TWENTY_MINUTE_DURATION_MILLIS))
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-
-         */
-
-        // perform action 5.1 //TODO missing in Testdefinition (save transaction)
-        // save the transaction and the changes made
-        CoroutineScope(Dispatchers.Main).launch {
-            instituteRepo.saveTransaction()
-        }
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-
-        /*
-        // assert only 2 slots are saved in managementDB
-        assert(instituteRepo.getObservableTimeSlotList().value != null)
-        var allClientSlots = instituteRepo.getObservableTimeSlotList().value!!
-            .filterIsInstance<ClientTimeSlot>()
-        assert(allClientSlots.size == 2)
-
-         */
-
-        // retrieve the slotCode of the added slots
-        for (clientSlot in allClientSlots) {
-            if (clientSlot.auxiliaryIdentifier == FIRST_SLOT_IDENTIFIER) {
-                firstSlotCode = clientSlot.slotCode
-            }
-            if (clientSlot.auxiliaryIdentifier == SECOND_SLOT_IDENTIFIER) {
-                secondSlotCode = clientSlot.slotCode
-            }
-        }
-
-        // perform action 5.1 (logout of management to get to loginFragment) //TODO missing in TestDef.
-        // logout of management
-        CoroutineScope(Dispatchers.Main).launch {
-            instituteRepo.logout()
-        }
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-
-        // check that we are logged out
-        assert(!instituteRepo.isLoggedIn().value!!)
-
-        // perform action 6 (enter second slotCode as client)
-        /*
-        CoroutineScope(Dispatchers.Main).launch {
-            clientRepo.newCodeEntered(secondSlotCode)
-        }
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-
-         */
-
-        onView(withId(R.id.etSlotCode))
-            .perform(TextSetter.setTextEditText(secondSlotCode), ViewActions.closeSoftKeyboard())
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-
-
-
-
-        // check if we navigated to remainingTimeFragment
-        onView(withId(R.id.btnRefresh)).check(
-            ViewAssertions.matches(
-                ViewMatchers.isDisplayed()
-            )
-        )
-
-
-        // login as management to change queue
-        // perform login
-        instituteRepo.login(
-            VALID_TEST_USERNAME,
-            VALID_TEST_PASSWORD
-        )
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-
-        // check that we are logged in
-        assert(instituteRepo.isLoggedIn().value!!)
-
-        // perform action 7
-        instituteRepo.newSpontaneousSlot(THIRD_SLOT_IDENTIFIER, Duration(
-            THIRTY_FIVE_MINUTE_DURATION_MILLIS))
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-        // save the transaction and the changes made
-        CoroutineScope(Dispatchers.Main).launch {
-            instituteRepo.saveTransaction()
-        }
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-
-        // assert only 3 slots are saved in managementDB
-        assert(instituteRepo.getObservableTimeSlotList().value != null)
-        allClientSlots = instituteRepo.getObservableTimeSlotList().value!!
-            .filterIsInstance<ClientTimeSlot>()
-        assert(allClientSlots.size == 3)
-
-        // retrieve the generated slotCode of the third slot
-        for (clientSlot in allClientSlots) {
-            if (clientSlot.auxiliaryIdentifier == THIRD_SLOT_IDENTIFIER) {
-                thirdSlotCode = clientSlot.slotCode
-            }
-        }
-
-        // perform action 8 (move third slot between first and second slot)
-        instituteRepo.moveSlotAfterAnother(thirdSlotCode, firstSlotCode)
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-
-        // save the transaction and the changes made (action 8.2 //TODO missing in TestDef)
-        CoroutineScope(Dispatchers.Main).launch {
-            instituteRepo.saveTransaction()
-        }
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-
-        // logout of management
-        CoroutineScope(Dispatchers.Main).launch {
-            instituteRepo.logout()
-        }
-        Thread.sleep(WAIT_RESPONSE_SERVER_LONG)
-
-        // check that we are logged out
-        assert(!instituteRepo.isLoggedIn().value!!)
-
-        // check for result
-        // TODO Checken dass delay notification erzeugt wurde
-
-        verify (exactly = 1)
-        { injectedManager.notify(DELAY_NOTIFICATION_ID, any()) }
-
-         */
     }
 
 }
