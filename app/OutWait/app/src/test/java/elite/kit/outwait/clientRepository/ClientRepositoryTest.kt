1f53b01513cd5abadd3c799296022beba3725a53
Benjamin Hänsel <benjamin.haensel@benjaminhaensel.de>
2021-03-26 17:22:58 +0100

    Comment cli repo tests and Fake

--- app/OutWait/app/src/test/java/elite/kit/outwait/clientRepository/ClientRepositoryTest.kt
+++ app/OutWait/app/src/test/java/elite/kit/outwait/clientRepository/ClientRepositoryTest.kt
@@ -21,73 +21,122 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
-
+/**
+ * Tests the behaviour of the client repository. Contains an advanced Fake
+ * for "the world", or more exactly for the internet and the database. So the
+ * complete backend is mocked and faked and this is a pure unit test.
+ *
+ */
 class ClientRepositoryTest {
 
-    val testDispatcher = TestCoroutineDispatcher()
-    val testCoroutineScope = TestCoroutineScope(testDispatcher)
-
-    private lateinit var repository: ClientRepository
-    private lateinit var internetAndDatabase: InternetAndDatabaseFake
-    private lateinit var serviceHandler: ServiceHandler
-
-
+    //Coroutine Test Environment setup
+    private val testDispatcher = TestCoroutineDispatcher()
+    private val testCoroutineScope = TestCoroutineScope(testDispatcher)
     @get: Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    //Mocks & Fakes
+    private lateinit var webDBFake: InternetAndDatabaseFake
+    private lateinit var serviceHandler: ServiceHandler
+
+    //the subject of the test
+    private lateinit var repo: ClientRepository
+
+
+
     @Before
     fun setUp(){
-
+        //Coroutine Test Environment setup
         Dispatchers.setMain(testDispatcher)
 
+        //Mocks & Fakes setUp
+        /*
+        we surround our Fake with a "spyk" so we are able to proof if certain methods
+        were called or not (we can just have all the cool mokk functions for our own
+        Fake)
+         */
 
-        internetAndDatabase = InternetAndDatabaseFake()
-
+        webDBFake = spyk(InternetAndDatabaseFake())
         serviceHandler = mockk<ServiceHandler>(relaxed = true)
 
-        repository = ClientRepository(internetAndDatabase, internetAndDatabase, serviceHandler)
+        //test subject setup
+        repo = ClientRepository(webDBFake, webDBFake, serviceHandler)
     }
 
     @After
     fun tearDown(){
+        //Coroutine Test Environment clean up
         Dispatchers.resetMain()
         testCoroutineScope.cleanupTestCoroutines()
     }
 
+    /**
+     * when first slotCode is entered, the repo tells the Service Handler to start
+     * the background actualization, but only if the entered code is correct
+     * (elsewise there is no slot we are interested to observe)
+     *
+     */
     @Test
-    fun `when first slotCode is entered, the repo tells the Service Handler to start (with correct slot code)`()
-    = runBlocking{
-        repository.newCodeEntered(internetAndDatabase.VALID_CODE)
+    fun `valid wait code entered - background service started`() = runBlocking{
+        repo.newCodeEntered(InternetAndDatabaseFake.VALID_CODE)
 
         verify(exactly = 1) { serviceHandler.startTimerService(any()) }
     }
 
+    /**
+     * when first slotCode is entered, the repo tells the Service Handler to start
+     * the background actualization, but only if the entered code is correct
+     * (elsewise there is no slot we are interested to observe)
+     *
+     */
     @Test
-    fun `receives slot data`()
-        = runBlocking{
-        repository.getActiveSlots().observeForever {  }
+    fun `invalid wait code entered - background service not started`() = runBlocking{
+        repo.newCodeEntered(InternetAndDatabaseFake.INVALID_CODE)
 
-        repository.newCodeEntered(internetAndDatabase.VALID_CODE)
+        verify(exactly = 0) { serviceHandler.startTimerService(any()) }
+    }
 
-        assertTrue(repository.getActiveSlots().value !== null)
-        assertTrue(repository.getActiveSlots().value!!.isNotEmpty())
-        assertEquals(internetAndDatabase.VALID_CODE, repository.getActiveSlots().value!!.first().slotCode)
+    /**
+     * After a successful wait code entry, the repo should actualize its live
+     * data with the received information from the server.
+     *
+     */
+    @Test
+    fun `receives slot data`() = runBlocking{
+        //Life data is lazy, so you have to observe it, elsewise it won´t change
+        repo.getActiveSlots().observeForever {  }
+
+        //enter valid slot code (server and db background stuff happens in fake now)
+        repo.newCodeEntered(InternetAndDatabaseFake.VALID_CODE)
+
+        //check if repo actualized live data with new client Info received from server
+        assertTrue(repo.getActiveSlots().value !== null)
+        assertTrue(repo.getActiveSlots().value!!.isNotEmpty())
+        assertEquals(InternetAndDatabaseFake.VALID_CODE, repo.getActiveSlots().value!!.first().slotCode)
+    }
+
+    /**
+     * check if repo maintains connection to server after valid slot code entry
+     *
+     */
+    @Test
+    fun `valid wait code entered - server connection maintained`() = runBlocking{
+        //in the beginning there must not be an established connection
+        assertFalse(repo.isConnectedToServer())
+        repo.newCodeEntered(InternetAndDatabaseFake.VALID_CODE)
+        //now there must be an established connection
+        assertTrue(repo.isConnectedToServer())
+        verify { webDBFake.initCommunication() }
     }
 
     @Test
-    fun `repo keeps connection to server after successful slot code entry`()
-        = runBlocking{
-        assertFalse(repository.isConnectedToServer())
-        repository.newCodeEntered(internetAndDatabase.VALID_CODE)
-        assertTrue(repository.isConnectedToServer())
-    }
-
-    @Test
-    fun `repo ends connection to server after slot is expired`()
-        = runBlocking{
-        repository.newCodeEntered(internetAndDatabase.VALID_CODE)
-        internetAndDatabase.expireAllSlots()
-        assertFalse(repository.isConnectedToServer())
+    fun `waiting slot expires - repo shuts down server connection`() = runBlocking{
+        repo.newCodeEntered(InternetAndDatabaseFake.VALID_CODE)
+        //tell background fake to make slot expire
+        webDBFake.expireAllSlots()
+        //after last active slot expired repo kills connection
+        assertFalse(repo.isConnectedToServer())
+        verify { webDBFake.endCommunication() }
     }
 
 
@@ -102,9 +151,23 @@ class ClientRepositoryTest {
      *
      */
     private class InternetAndDatabaseFake : ClientHandler, ClientInfoDao {
-        val VALID_CODE = "V"
-        val INVALID_CODE = "I"
 
+        companion object{
+            /**
+             * Valid slot code that leads to live data change
+             */
+            const val VALID_CODE = "V"
+
+            /**
+             * invalid slot code --> error message
+             */
+            const val INVALID_CODE = "I"
+        }
+
+        /**
+         * makes all slots expireing (as if the server would send endSlot@C
+         *
+         */
         fun expireAllSlots(){
             errorMessages.value = errorMessages.value !!+ ClientServerErrors.EXPIRED_SLOT_CODE
             clientInfoList.value = listOf()
@@ -121,6 +184,12 @@ class ClientRepositoryTest {
             return true
         }
 
+        /**
+         * reacts depending on the slot code in the companion object.
+         * elsewise behaviour not specified
+         *
+         * @param slotCode see slot codes in companion object
+         */
         override fun newCodeEntered(slotCode: String) {
             when (slotCode){
                 VALID_CODE
@@ -134,6 +203,9 @@ class ClientRepositoryTest {
                         Duration(0)
                     )
                 )
+                INVALID_CODE
+                -> errorMessages.value = errorMessages.value !!+ ClientServerErrors.INVALID_SLOT_CODE
+
             }
         }
 
@@ -141,45 +213,19 @@ class ClientRepositoryTest {
             TODO("Not yet implemented")
         }
 
-        override fun getErrors(): LiveData<List<ClientServerErrors>>
-            = errorMessages
-
         private val errorMessages = MutableLiveData<List<ClientServerErrors>>(listOf())
+        override fun getErrors() = errorMessages as LiveData<List<ClientServerErrors>>
 
-
-        override fun getAllClientInfoObservable(): LiveData<List<ClientInfo>> {
-            return clientInfoList
-        }
         private val clientInfoList = MutableLiveData<List<ClientInfo>>(listOf())
+        override fun getAllClientInfoObservable() = clientInfoList as LiveData<List<ClientInfo>>
 
-        override fun insert(clientInfo: ClientInfo) {
-            throw NotImplementedError()
-        }
-
-        override fun update(clientInfo: ClientInfo) {
-            throw NotImplementedError()
-        }
-
-        override fun getClientInfo(waitCode: String): ClientInfo? {
-            throw NotImplementedError()
-        }
-
-        override fun deleteClientInfo(info: ClientInfo) {
-            throw NotImplementedError()
-        }
-
-        override fun getClientInfoObservable(slotCode: String): LiveData<ClientInfo?> {
-            throw NotImplementedError()
-        }
-
-        override fun getAllClientInfo(): List<ClientInfo> {
-            throw NotImplementedError()
-        }
-
-
-        override fun clearTable() {
-            throw NotImplementedError()
-        }
+        override fun insert(clientInfo: ClientInfo) = throw NotImplementedError()
+        override fun update(clientInfo: ClientInfo) = throw NotImplementedError()
+        override fun getClientInfo(waitCode: String): ClientInfo? = throw NotImplementedError()
+        override fun deleteClientInfo(info: ClientInfo) = throw NotImplementedError()
+        override fun getClientInfoObservable(slotCode: String): LiveData<ClientInfo?> = throw NotImplementedError()
+        override fun getAllClientInfo(): List<ClientInfo> = throw NotImplementedError()
+        override fun clearTable() = throw NotImplementedError()
 
     }
 }
